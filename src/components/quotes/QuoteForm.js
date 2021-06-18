86608990c1a402a85eac4211e7caf3db7b70247d
juliodiaz <julio@vimbly.com>
2021-06-17 21:20:00 -0500

    Using Prompt component

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,4 +1,5 @@
-import { useRef } from 'react';
+import { Fragment, useRef, useState } from 'react';
+import { Prompt } from 'react-router-dom';
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
@@ -7,6 +8,7 @@ import classes from './QuoteForm.module.css';
 const QuoteForm = (props) => {
   const authorInputRef = useRef();
   const textInputRef = useRef();
+  const [isEntered, setIsEntered] = useState(false);
 
   function submitFormHandler(event) {
     event.preventDefault();
@@ -19,9 +21,24 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
+  const addQuoteHandler = () => {
+    setIsEntered(false);
+  }
+
+  const formFocusedHandler = () => {
+    setIsEntered(true);
+  }
+
   return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
+    <Fragment>
+      <Prompt 
+        when={isEntered}
+        message={(location) => 'Are you sure yo want to leave?'} />
+      <Card>
+      <form 
+        onFocus={formFocusedHandler}
+        className={classes.form} 
+        onSubmit={submitFormHandler}>
         {props.isLoading && (
           <div className={classes.loading}>
             <LoadingSpinner />
@@ -30,17 +47,18 @@ const QuoteForm = (props) => {
 
         <div className={classes.control}>
           <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
+          <input required type='text' id='author' ref={authorInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
+          <textarea required id='text' rows='5' ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
+          <button onClick={addQuoteHandler} className='btn'>Add Quote</button>
         </div>
       </form>
     </Card>
+    </Fragment>
   );
 };
 
