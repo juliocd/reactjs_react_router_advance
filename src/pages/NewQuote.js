659ffe565b8d9c754c49826aea01a969fcf2276e
juliodiaz <julio@vimbly.com>
2021-06-17 20:59:43 -0500

    Adding Programatic navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,13 @@
+import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+    const history = useHistory();
     const addQuoteHandler = (quoteData) => {
         console.log(quoteData)
+
+        history.push('/quotes'); // Add a mew page to the history stack
+        //history.replace('/quotes'); // Redirect to a new page (Destroy last history stack)
     }
     
     return <QuoteForm onAddQuote={addQuoteHandler} />
